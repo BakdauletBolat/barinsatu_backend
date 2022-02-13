5fc7f9d761a1c5f6bdb399921953f1a990f61d39
Бакдаулет Болат <bakosh21345@gmail.com.com>
2022-02-14 05:35:19 +0600

    a

--- barinsatu/settings.py
+++ barinsatu/settings.py
@@ -71,22 +71,22 @@ WSGI_APPLICATION = 'barinsatu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'barinsatu',
-        'USER': 'bakdaulet',
-        'PASSWORD': 'baguvix123FFF',
-        'HOST': 'localhost',
-    }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'barinsatu',
+    'USER': 'bakdaulet',
+    'PASSWORD': 'baguvix123FFF',
+    'HOST': 'localhost',
+}
 }
 
 
