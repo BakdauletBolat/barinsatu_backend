eb83b6a3272fdade3ab13775ce8cde8a9a931579
Бакдаулет Болат <bakosh21345@gmail.com.com>
2022-03-04 23:16:35 +0600

    a

--- story/urls.py
+++ story/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import StoryListView,StoryCreateView,CommentCreateView,LikeStory
+from .views import StoryListView,StoryCreateView,CommentCreateView,LikeStory,ViewStory
 
 urlpatterns = [
     
     path('',StoryListView.as_view()),
     path('create/',StoryCreateView.as_view()),
     path('comment-create/',CommentCreateView.as_view()),
+    path('view/<int:pk>/',ViewStory.as_view()),
     path('like-story/<int:pk>/',LikeStory.as_view())
 ]
